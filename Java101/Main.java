57dbfaf8da0a1f627e5ea280d575c404010efa1a
DanyManz <danymanz2010@gmail.com>
2024-11-17 14:34:43 -0500

    Java101 second practice problem

--- Java101/Main.java
+++ Java101/Main.java
@@ -1,7 +1,11 @@
 public class Main {
 
 public static void main(String[] args) {
-   
+    int[] arr = {7, 1, 594, 3, 1482, 19, 12, 3};
+    int[] result = PracticeProblems.reverseArray(arr);
+    for (int r = 0; r < result.length; r++)
+    System.out.print(result[r] + " ");
+        
     
 }
 
--- Java101/PracticeProblems.java
+++ Java101/PracticeProblems.java
@@ -1,5 +1,5 @@
 public class PracticeProblems {
-   
+   //question 1
     public static int countOccurances(int[] arr, int n) {
         int Occurances = 0;
         for (int c = 0; c < arr.length; c++)
@@ -8,10 +8,20 @@ public class PracticeProblems {
         }
         return Occurances;
 }
-    public static void main(String[] args) {
-        int[] arr = {7, 1, 594, 3, 1482, 19, 12, 3};
-        int n = 3;
-        int result = countOccurances(arr, n);
-        System.out.println(n + " appears in arr " + result + " times" );
+
+    //question 2
+    public static int[] reverseArray(int[] arr) {
+        int[] reversed = new int[arr.length];
+        for (int r = 0; r < arr.length; r++) {
+        reversed[r] = arr[arr.length-1-r]; 
+        }
+    return reversed;
     }
+    public static void main(int[] args) {
+    int[] arr = {7, 1, 594, 3, 1482, 19, 12, 3};
+    int[] result = reverseArray(arr);
+    for (int r = 0; r < result.length; r++)
+    System.out.print(result[r]);
+    }
+
 }
