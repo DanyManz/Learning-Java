df19f415b910a744f41e873d79323e0110d71b63
DanielW254 <Danymanz2010@gmail.com>
2024-11-17 15:42:19 -0500

    Practice Problem 4

--- Java101/Main.java
+++ Java101/Main.java
@@ -1,8 +1,7 @@
 public class Main {
 
 public static void main(String[] args) {
-    int n = 10;
-    System.out.print("The " + n + "th number in the fibonacci sequence is " + PracticeProblems.fib(n));
+    
 
 }
 }
