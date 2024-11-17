2625ad58b28c881da8ed0824e55834e0885f93e1
DanielW254 <Danymanz2010@gmail.com>
2024-11-17 15:30:56 -0500

    Practice Problem 4

--- Java101/Main.java
+++ Java101/Main.java
@@ -1,14 +1,8 @@
 public class Main {
 
 public static void main(String[] args) {
-    double[][] grid = {
-    {3.1, 4.2, 2.7},
-    {1.2, 8.1, 0.7},
-    {6.3, 2.4, 1.3}
-    };
-    double result = PracticeProblems.sumGrid(grid);
-    System.out.print("The sum of all elements in the grid is " + result);
+ int n = 10;
+System.out.print("The " + n + "th number in the fibbonaci sequence is " + PracticeProblems.fib(n));
 
 }
-
 }
--- Java101/PracticeProblems.java
+++ Java101/PracticeProblems.java
@@ -26,7 +26,13 @@ public class PracticeProblems {
         }
         return sum;
     }
+    //question 4
+    public static int fib(int n) {
+        if (n == 0) return 0;
+        if (n == 1) return 1;
 
-
+        return fib(n-1) + fib(n-2);
+    
+    }
 
 }
