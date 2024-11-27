f495ec8759348e4c4d5791d797c21347e325a381
DanielW254 <Danymanz2010@gmail.com>
2024-11-26 22:18:11 -0500

    Practice: fromPoints

---
+++ Java102/Circle.java
@@ -0,0 +1,84 @@
+public class Circle implements Main.Shape {
+	
+    public final Point center;
+	public final double radius;
+
+	public Circle(Point center, double radius) {
+		this.center = center;
+		this.radius = radius;
+	}
+
+	public double area() {
+		return Math.PI * Math.pow(radius, 2);
+	}
+
+	public double perimeter() {
+		return 2 * Math.PI * radius;
+	}
+
+	/** 
+	* @return Whether point p is inside of the circle.
+	*/
+	public boolean isInside(Point p) {
+		return Point.distance(center, p) < radius;
+	}
+
+	/** 
+	* @return Whether point p part of/on the border of the circle.
+	*/
+	public boolean isOn(Point p) {
+		return Point.distance(center, p) == radius;
+	}
+
+	/** 
+	* @param x How much to translate the circle by in the + x direction.
+	* @param y How much to translate the circle by in the + y direction.
+	* @return The circle that results from the translation.
+	*/
+	public Circle translate(double x, double y) {
+		return new Circle(center.translateX(x).translateY(y), radius);
+	}
+
+	/** 
+	* @return The circle that results from scaling by k.
+	*/
+	public Circle scale(double k) {
+		return new Circle(center, radius * k);
+	}
+
+	@Override
+	public String toString() {
+		return "(center: " + center + "; radius: " + radius + ")";
+	}
+
+    //Practice: fromPoints
+    public Point getCenter() {
+        return center;
+    }
+
+    public double getRadius() {
+        return radius;
+    }
+    public static Circle fromPoints(Point p1, Point p2, Point p3) {
+        double x1 = p1.getX();
+        double y1 = p1.getY();
+
+        double x2 = p2.getX();
+        double y2 = p2.getY();
+
+        double x3 = p3.getX();
+        double y3 = p3.getY();
+    
+        //I don't really understand what is going on here
+        double D = 2 * (x1 * (y2 - y3) + x2 * (y3 - y1) + x3 * (y1 - y2));
+        double newCenterX = ((x1 * x1 + y1 * y1) * (y2 - y3) + (x2 * x2 + y2 * y2) * (y3 - y1) + (x3 * x3 + y3 * y3) * (y1 - y2)) / D;
+        double newCenterY = ((x1 * x1 + y1 * y1) * (x3 - x2) + (x2 * x2 + y2 * y2) * (x1 - x3) + (x3 * x3 + y3 * y3) * (x2 - x1)) / D;
+        
+        Point newCircleCenter = new Point(newCenterX, newCenterY);
+        double newCircleRadius = Point.distance(newCircleCenter, p1);
+
+        return new Circle(newCircleCenter, newCircleRadius);
+
+
+    }
+}
--- Java102/Main.java
+++ Java102/Main.java
@@ -1,6 +1,53 @@
 
 public class Main {
+        public interface Shape {
+        public double area();
+    
+        public double perimeter();
+    
+        /** 
+        * @return Whether point p is inside of the shape.
+        */
+        public boolean isInside(Point p);
+        /** 
+        * @return Whether point p part of/on the border of the shape.
+        */
+        public boolean isOn(Point p);
+    
+        /** 
+        * @param x How much to translate the shape by in the + x direction.
+        * @param y How much to translate the shape by in the + y direction.
+        * @return The shape that results from the translation.
+        */
+        public Shape translate(double x, double y);
+    
+        /** 
+        * @return The shape that results from scaling by k.
+        */
+        public Shape scale(double k);
+    }
+
+    static double sumArea(Shape[] shapes) {
+        double sum = 0;
+        for (Shape shape : shapes) {
+            sum += shape.area();
+        }
+        return sum;
+    }
+    static Shape[] scaleAll(Shape[] shapes, double k) {
+        Shape[] scaled = new Shape[shapes.length];
+        for (int i = 0; i < shapes.length; i++) {
+            scaled[i] = shapes[i].scale(k);
+        }
+        return scaled;
+    }
     public static void main(String[] args) {
+      Point p1 = new Point(10, 0);
+      Point p2 = new Point(0, 5);
+      Point p3 = new Point(0, 15);
+        Circle circle = Circle.fromPoints(p1, p2, p3);
+        System.out.println("New circle center point: (" + circle.getCenter().getX() + ", " + circle.getCenter().getY() + ")");
+        System.out.println("New circle radius: " + circle.getRadius());
 
     }
 }
